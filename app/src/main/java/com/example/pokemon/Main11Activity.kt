264dc4c0cba196511f778b8208b0ff3deebc6004
LeoM98 <54753222+LeoM98@users.noreply.github.com>
2019-11-29 10:48:08 -0500

    Update Main11Activity.kt

--- app/src/main/java/com/example/pokemon/Main11Activity.kt
+++ app/src/main/java/com/example/pokemon/Main11Activity.kt
@@ -2,11 +2,14 @@ package com.example.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main11.*
 
 class Main11Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main11)
+
+        btn3.text=MainActivity().obtuser()
     }
 }
